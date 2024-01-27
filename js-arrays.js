ab50006f02d9ef7a2f02541c7bcd3a039ae95533
Andrey Vlasov <98467872+crotchety77@users.noreply.github.com>
2024-01-27 18:09:03 +0300

    pomadoro-2
    indexOf, includes, split and join, reverse + task7-1

--- js-arrays.js
+++ js-arrays.js
@@ -1,5 +1,5 @@
 // Методы массивов
-// Splice Цель удалить элементы из массива и вставить другие
+// 1.Splice Цель удалить элементы из массива и вставить другие
 
 let lessons = ["Math", "AVS", "OS", "English"];
 
@@ -12,7 +12,7 @@ lessons.splice(1,2, "Test", "Program");
 
 
 
-// Slice Обрезка
+// 2.Slice Обрезка
 let agesOfClients = [25, 32, 19, 40];
 
 // Неизменяет текующий массив
@@ -20,5 +20,42 @@ let agesOfClients = [25, 32, 19, 40];
 let test = agesOfClients.slice(1,4);
 
 // 1, 3 -> [32, 19]
-console.log(agesOfClients.splice(1,3));
-console.log(test);
+// 1, 4 -> [32, 19, 40]
+// console.log(agesOfClients);
+// console.log(test);
+
+// 3.indexOf
+// Существует ли в массиве значение?
+
+let food = ["Яблоко", "Манго", "Апельсин", "Дыня"];
+
+// console.log(food.indexOf("Апельсин")); // 2
+// console.log(food.indexOf("Шоколад")); // -1
+
+// 4.includes
+
+let technologies = ["Html", "CSS", "JS"];
+
+// console.log(technologies.includes("Html")); // True
+// console.log(technologies.includes("Docker")); // False
+
+// 5. split (метод строк) но мы можем использовать в массивах
+
+let list = "Кроссовки, джинсы, рюкзак, фотоаппарат";
+let listArray = list.split(", ");
+console.log(listArray);
+
+let string = "Кроссовка";
+console.log(string.split(""));
+
+// 6. join объединение массива -> строка
+
+let list2 = listArray.join("-");
+console.log(list2);
+// Кроссовки-джинсы-рюкзак-фотоаппарат
+
+// 7. reverse
+
+// Изменяет текущий массив
+console.log(technologies.reverse());
+console.log(technologies);
