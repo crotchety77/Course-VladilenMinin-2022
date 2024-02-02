0c37d3c139075c2fb677d6838fb4b366d3314a2a
Andrey Vlasov <98467872+crotchety77@users.noreply.github.com>
2024-02-02 20:38:21 +0300

    pomadoro-13
    objects-2-task. Решил просто. Потом глянул ответ. Долго думал. Влад скинул своё решение через find. Надо больше заданий через map, reduce etc.

--- js-objects-task-2.js
+++ js-objects-task-2.js
@@ -11,6 +11,7 @@
 // отсортированы по id из массива orders.
 // Посмотрите на возможный результат функции giveTalonsInOrder:
 const ordersArr = [4, 2, 1, 3];
+// const ordersArr = [3,1,0,2];
 const people = [
 { id: 1, name: "Максим" },
 { id: 2, name: "Николай" },
@@ -18,17 +19,59 @@ const people = [
 { id: 4, name: "Виталий" },
 ];
 
-function giveTalonsInOrder(people, arr){
-    const result = [];
-    arr.forEach(number => {
-        // console.log(number);
-        console.log(people.id['number']);
-    });
-    return result;
+function giveTalonsInOrder(people, ordersArr){
+    // console.log(people[0].id);
+    return ordersArr.map((order) => people[order-1]);
+
+        // return orders.map((order) => objectWithIndexes[order]);
 }
 
 const result = giveTalonsInOrder(people, ordersArr);
 console.log('result', result);
+
+// const people2 = { 
+//     1: { id: 1, name: "Максим" },
+//     2: { id: 2, name: "Николай" },
+//     3: { id: 3, name: "Ангелина" },
+//     4: { id: 4, name: "Виталий" },
+//     };
+
+// function giveTalonsInOrder(people, arr){
+//     let result = [];
+//     let i = 0
+//     while (i != 4){
+//         people.forEach(element => {
+//             if (element.id === arr[i]){
+//                 result.push(element);
+//             }
+//         })
+//         i++
+//     }
+//     return result;
+// }
+
+// function giveTalonsInOrder(people, arr){
+//     let result = [];
+//     arr.forEach(obj => {
+//         // console.log(people[0/], obj)
+//         result.push(people.find(person => person.id === obj))
+//     });
+//     return result
+// }
+
+// const giveTalonsInOrder = (patients, orders) => {
+//     const objectWithIndexes = patients.reduce((acc, curPerson) => {
+//         acc[curPerson.id] = curPerson;
+//         return acc;
+//     }, {});
+//     return orders.map((order) => objectWithIndexes[order]);
+// }
+
+// const result = giveTalonsInOrder(people, ordersArr);
+// console.log('result', result);
+
+
+
 // console.log(1);
 /* Возвращает массив
 [
