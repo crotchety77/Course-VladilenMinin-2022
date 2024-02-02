d394853f9a29fee84fc8b1173fa52eafdc5cfef8
Andrey Vlasov <98467872+crotchety77@users.noreply.github.com>
2024-02-02 18:11:25 +0300

    pomadoro-11
    complete arrays-tasks

---
+++ js-arrays-task-10.js
@@ -0,0 +1,11 @@
+console.log('task 10');
+
+const matrix = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
+];
+
+let arrayMatrix = matrix[0].concat(matrix[1], matrix[2]);
+console.log(arrayMatrix);
+console.log(matrix);
